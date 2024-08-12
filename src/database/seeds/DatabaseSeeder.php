630f452615a65d1169da2f5e9229828b5b679123
marmous <yoann.calamai@marmous.net>
2024-08-12 18:17:41 +0200

    add default value for db

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // create default user
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@localhost',
+            'password' => bcrypt('password'),
+            'email_verified_at' => now(),
+        ]);
 
+        // create default content for legal mentions
+        DB::table('contenuses')->insert([
+            'name' => 'mentionslegales',
+            'contenu' => '',
+        ]);
+
+        // create default content for help
+        DB::table('contenuses')->insert([
+            'name' => 'aide',
+            'contenu' => '',
+        ]);
+
+        // create default content for changelog
+        DB::table('contenuses')->insert([
+            'name' => 'changelog',
+            'contenu' => '',
+        ]);
+
+        // create default content for future
+        DB::table('contenuses')->insert([
+            'name' => 'avenir',
+            'contenu' => '',
+        ]);
+
+        // create default content for game creation
+        DB::table('contenuses')->insert([
+            'name' => 'creerjeu',
+            'contenu' => '',
+        ]);
 
     }
 }
